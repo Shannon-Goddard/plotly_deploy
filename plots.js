9879821a0fb385260c87f1a3498b948caf6ac164
Shannon Goddard <Uthingkithunk@aol.com>
2020-01-02 18:25:55 -0800

    Add files via upload

--- plots.js
+++ plots.js
@@ -27,44 +27,15 @@ function buildMetadata(sample) {
     var PANEL = d3.select("#sample-metadata");
 
     PANEL.html("");
-    PANEL.append("h6").text(result.location);
+    PANEL.append("h6").text(["ID:", result.id]);
+    PANEL.append("h6").text(["ETHNICITY:", result.ethnicity]);
+    PANEL.append("h6").text(["GENDER:", result.gender]);
+    PANEL.append("h6").text(["AGE:", result.age]);
+    PANEL.append("h6").text(["LOCATION:", result.location]);
+    PANEL.append("h6").text(["BBTYPE:", result.bbtype]);
+    PANEL.append("h6").text(["WFREQ:", result.wfreq]);
   });
 }
 
-// Sort the data array using the otu_labels value
-data.sort(function(a, b) {
-  return parseFloat(b.otu_labels) - parseFloat(a.otu_labels);
-});
 
-// Slice the first 10 objects for plotting
-data = data.slice(0, 10);
 
-// Reverse the array due to Plotly's defaults
-data = data.reverse();
-
-// Trace1 for the otu Data
-var trace1 = {
-  x: data.map(row => row.otu_labels),
-  y: data.map(row => row.id),
-  text: data.map(row => row.id),
-  name: "otu",
-  type: "bar",
-  orientation: "h"
-};
-
-// data
-var data = [trace1];
-
-// Apply the group bar mode to the layout
-var layout = {
-  title: "Top 10 OTUs",
-  margin: {
-    l: 100,
-    r: 100,
-    t: 100,
-    b: 100
-  }
-};
-
-// Render the plot to the div tag with id "plot"
-Plotly.newPlot("bar", data, layout);
